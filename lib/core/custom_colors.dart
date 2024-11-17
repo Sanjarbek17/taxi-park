4e826c22593a69cb3d59a923de9daf7a4976ad3c
Sanjarbek17 <sanjarsaidov173@gmail.com>
2024-11-17 21:50:21 +0500

    splash logo

--- lib/core/custom_colors.dart
+++ lib/core/custom_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class CustomColors {
   static const Color primaryColor = Color(0xffEDAE10);
+  static const Color secondaryColor = Color(0xffFEC400);
 }
--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:taxi_park/core/custom_colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      // backgroundColor: ,
+    return  Scaffold(
+      backgroundColor: CustomColors.secondaryColor,
       body: Center(
-        child: Text('Splash Screen'),
+        child: SvgPicture.asset('assets/icons/logo.svg'),
       ),
     );
   }
