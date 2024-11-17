a6c41647d9a92f0e22aa07d56ae8c967cb0037fa
Sanjarbek17 <sanjarsaidov173@gmail.com>
2024-11-17 12:17:11 +0500

    dependency injection

---
+++ lib/depency_injection.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taxi_park/data/repository/data_repo.dart';
+import 'package:taxi_park/data/services/remote_service.dart';
+import 'package:taxi_park/presentation/blocs/orders/orders_bloc.dart';
+
+final GetIt locator = GetIt.instance;
+
+Future<void> setupLocator() async {
+  final pref = await SharedPreferences.getInstance();
+  final dio = Dio();
+
+  // Registering the Instances
+  locator.registerLazySingleton(() => pref);
+  locator.registerLazySingleton(() => dio);
+
+  // Registering the Services
+  locator.registerLazySingleton(
+    () => RemoteService(
+      dio: locator(),
+      sharedPreferences: locator(),
+    ),
+  );
+
+  // Registering the Repository
+  locator.registerLazySingleton(
+    () => DataRepo(remoteService: locator()),
+  );
+
+  // Registering the Bloc
+  locator.registerLazySingleton(
+    () => OrdersBloc(ordersRepository: locator<DataRepo>()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:taxi_park/core/custom_theme.dart';
+import 'package:taxi_park/depency_injection.dart';
+import 'package:taxi_park/presentation/blocs/orders/orders_bloc.dart';
 import 'package:taxi_park/presentation/pages/app_view.dart';
 import 'package:taxi_park/presentation/pages/login_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await setupLocator();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => locator<OrdersBloc>(),
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -24,9 +42,6 @@ class _MyAppState extends State<MyApp> {
       routes: {
         '/': (context) => const LoginPage(),
         '/home': (context) => const AppView(),
-        // '/users': (context) => const DriversPage(),
-        // '/map': (context) => const MapPage(),
-        // '/orders': (context) => const OrdersPage(),
       },
     );
   }
